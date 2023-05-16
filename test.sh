6b7d0fbd2304f6883a611650f89bb2b8c3341a91
Muthu ramalingam <128946991+Muthuinc@users.noreply.github.com>
2023-05-16 11:51:46 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -93,24 +93,9 @@ echo  "    container_name: test2 "  >> docker-compose.yml
 echo  "    ports:"  >> docker-compose.yml
 echo  '      - "80:80"'  >> docker-compose.yml
 echo  "    volumes:"  >> docker-compose.yml
-echo  "      - ./ :/var/www/html"  >> docker-compose.yml
+echo  "      - ./:/var/www/html"  >> docker-compose.yml
 
-# creating docker container
 
-docker-compose up -d 
-
-# checking whether the port is open or not
-
-if curl http://localhost:80 ;
-  then
-   echo " port is running"
-   docker ps
-else 
-   echo " port is not running"
-   docker ps -a
-fi
-
-# if port is not running after the image is built maybe the problem is in CMD line please check
 
 
 
