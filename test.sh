1801976ec5c304ea473981a3931190d2822c671f
Muthu ramalingam <128946991+Muthuinc@users.noreply.github.com>
2023-05-16 11:05:15 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,72 @@ set -e
 
 
 touch index.html
-echo "successfully build" > index.html
+echo "<!DOCTYPE html>
+<html>
+<head>
+	<title> day2 </title>
+</head>
+
+<body>
+
+<!-- ordered list -->
+
+<ol>
+    <li> first line</li>
+    <li> second line </li>
+</ol>
+
+<!-- trying italic format -->
+
+<p> this is the example of <em> italic </em> format </p>
+
+<!-- trying bold -->
+
+<p> i am <b>muthu</b> </p>
+
+<!-- trying underline -->
+
+<p> the line <u> line</u> </p>
+
+<!-- Description list -->
+
+<dl>
+   <dt> about html </dt><br>
+   <dd> html is the website designing language </dd>
+</dl>
+
+<!-- abbreviation title -->
+
+<abbr title="bachelor of engineering">B.E</abbr>
+
+<!-- superscript -->
+
+<p> hellow good morning time is 7<sup>o</sup> clock </p> 
+
+<!-- subscript -->
+
+<p> chemical formula of water is H<sub>2</sub>O </p>
+
+<!-- time  need to know more -->
+
+<p> what date is today? <br>
+    today is  04 May 2023 </time> </p>
+    
+<!-- adding link to images -->
+
+<a href="https:www.google.com">
+<img src="nature.jpeg" alt="nature of the image"> </img> </a>
+
+<p>
+<address>
+xxx
+xxx
+yyy
+yyy
+</address>
+
+    
+     " > index.html
 
 
 # creating Dockerfile
